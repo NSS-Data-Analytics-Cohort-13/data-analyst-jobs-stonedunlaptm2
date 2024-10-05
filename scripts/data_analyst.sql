56de780a0702bc77f93c69f6f2c8cbc4cca591c7
stonedunlaptm2 <tstone112307@gmail.com>
2024-10-05 13:25:27 -0500

    completed with updates

--- scripts/data_analyst.sql
+++ scripts/data_analyst.sql
@@ -2,24 +2,44 @@
 
 SELECT COUNT(*)
 FROM data_analyst_jobs;
+--Answer:1793
 
---2.Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? Company:"XTO Land Data Analyst"
+--2.Write a query to look at just the first 10 rows. What company is associated with the job posting on the 10th row? Company:"ExxonMobil"
 
 SELECT *
 FROM data_analyst_jobs
 LIMIT 10;
+--Answer="ExxonMobil"
 
---3.How many postings are in Tennessee?-21 How many are there in either Tennessee or Kentucky?-27
+--3.How many postings are in Tennessee?- How many are there in either Tennessee or Kentucky?-
+SELECT COUNT(company)
+FROM data_analyst_jobs
+WHERE location='TN'
+--Answer=21
 
 SELECT COUNT(company)
 FROM data_analyst_jobs
 WHERE location='TN' OR location='KY'
-
---4.How many postings in Tennessee have a star rating above 4?-416
-
-SELECT COUNT(company)
+--OR
+SELECT COUNT (*)
 FROM data_analyst_jobs
-WHERE star_rating >4;
+WHERE location IN ('TN','KY');
+--Answer= 27
+
+--4.How many postings in Tennessee have a star rating above 4?
+
+SELECT *
+FROM data_analyst_jobs
+WHERE (location='TN') AND star_rating >4;
+--Answer=3
+
+OR 
+
+SELECT COUNT (location)
+FROM data_analyst_jobs
+WHERE (location='TN') AND star_rating >4;
+--Answer=3
+
 
 --5.How many postings in the dataset have a review count between 500 and 1000?--151
 
@@ -27,54 +47,61 @@ SELECT COUNT (company)
 FROM data_analyst_jobs
 WHERE review_count
 BETWEEN 500 and 1000;
+--Answer=151
 
 --6.Show the average star rating for companies in each state. The output should show the state as `state` and the average rating for the state as `avg_rating`. Which state shows the highest average rating?
 
 SELECT location, AVG(star_rating) AS avg_rating
 FROM data_analyst_jobs
-GROUP BY location;
+GROUP BY location
+ORDER BY avg_rating ASC;
+--Answer=NE
 
---7.Select unique job titles from the data_analyst_jobs table. How many are there?-881
+--7.Select unique job titles from the data_analyst_jobs table. How many are there?
 SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs;
+--Answer: 881
 
---8.How many unique job titles are there for California companies?-230
+--8.How many unique job titles are there for California companies?
 SELECT COUNT(DISTINCT title)
 FROM data_analyst_jobs
 WHERE location='CA';
+--Answer: 230
 
---9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?-95
+--9.Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-SELECT title, COUNT(AVG star_rating)
+SELECT company, AVG(star_rating) AS avg_star_rating
 FROM data_analyst_jobs
-WHERE review_count>5000
-GROUP BY title;
+WHERE company IS NOT NULL
+GROUP BY company
+HAVING MIN(review_count) >5000;
+
+--Answer: 40
 
 --10.Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
-SELECT title, COUNT(AVG star_rating)
+SELECT company,
+	   AVG(star_rating) AS avg_star_rating
 FROM data_analyst_jobs
-WHERE review_count>5000
-GROUP BY title
-ORDER BY AVG star_rating ASC
+WHERE company IS NOT NULL
+GROUP BY company
+HAVING MIN(review_count) > 5000
+ORDER BY avg_star_rating DESC;
+--Answer 6 way tie
 
---11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? -1636
-SELECT COUNT(title)
+--11.Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
+SELECT DISTINCT(title)
 FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
-
-SELECT (title)
-FROM data_analyst_jobs
-WHERE title LIKE '%Analyst%';
+WHERE title ILIKE '%Analyst%';
+--Answer 774
 
 
---12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?-157; These positions still have the word analyst in common but te capitalization is different
-SELECT COUNT(title)
-FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%';
+--12.How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common? These positions still have the word analyst in common but the capitalization is different
 
 SELECT (title)
 FROM data_analyst_jobs
-WHERE title NOT LIKE '%Analyst%';
+WHERE title NOT ILIKE '%analyst%'
+AND title NOT ILIKE '%analytics%';
+--Answer 4
 
 
 --Bonus
@@ -84,7 +111,11 @@ WHERE title NOT LIKE '%Analyst%';
   -- Which three industries are in the top 3 on this list? -"Consulting and Business Services", "Computers and Electronics", "Real Estate"
   --How many jobs have been listed for more than 3 weeks for each of the top 3?-
 
-  SELECT *
-  FROM data_analyst_jobs
-  WHERE days_since_posting>21 AND skill='SQL'
-  ORDER BY days_since_posting DESC
+ SELECT COUNT (d.title), d.domain
+ FROM data_analyst_jobs as d
+ WHERE d.days_since_posting > 21
+ 		AND d.title IS NOT NULL
+		 AND d.domain IS NOT NULL
+		 AND skill ILIKE '%SQL%'
+GROUP BY d.domain 
+ORDER BY COUNT (d.title) DESC;
